a411db855e7c5d879f628fd906a0a231f4d0aa36
ading2210 <ading2019@gmail.com>
2024-03-09 20:03:41 -0800

    add build script, delete dist from git

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './BareTypes';
-export * from './BareClient';
-export * from './Switcher';
-export * from './RemoteClient';
-export { BareClient as default } from './BareClient';
-export { WebSocketFields } from "./snapshot";
