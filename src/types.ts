223ad3d2865e4f478f9d6d6534597767e403c392
Percs <83934299+Percslol@users.noreply.github.com>
2024-06-26 05:20:21 -0500

    actually export bare-mux types

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+export type * from './BareTypes';
+export type * from './BareClient';
+export type * from './Switcher';
+export type * from './RemoteClient';
+export type { BareClient as default } from './BareClient';
+export type { WebSocketFields } from "./snapshot";
